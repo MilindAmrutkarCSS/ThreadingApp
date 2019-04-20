c012cac9d428679ac56a020c363792793500d75f
Milind Amrutkar <cssindiamilind@gmail.com>
2019-04-20 12:43:27 +0530

    Done with tutorial 2.

--- app/src/main/java/com/example/threadingapp/MainActivity.java
+++ app/src/main/java/com/example/threadingapp/MainActivity.java
@@ -2,117 +2,60 @@ package com.example.threadingapp;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import static com.example.threadingapp.ExampleHandler.TASK_A;
+import static com.example.threadingapp.ExampleHandler.TASK_B;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private Button buttonStartThread;
-
-    private volatile boolean stopThread = false;
+    private ExampleLooperThread looperThread = new ExampleLooperThread();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        buttonStartThread = findViewById(R.id.btnStart);
     }
 
     public void startThread(View view) {
-        /*ExampleThread thread = new ExampleThread(20);
-        thread.start();*/
-
-        stopThread = false;
-        ExampleRunnable runnable = new ExampleRunnable(20);
-        new Thread(runnable).start();
-
-     /* new Thread(new Runnable() {
-          @Override
-          public void run() {
-
-          }
-      }).start();*/
-
+        looperThread.start();
     }
 
     public void stopThread(View view) {
-        stopThread = true;
+        looperThread.looper.quit();
     }
 
-    class ExampleThread extends Thread {
-        int seconds;
+    public void taskA(View view) {
 
-        public ExampleThread(int seconds) {
-            this.seconds = seconds;
-        }
+        Message msg = Message.obtain();
+        msg.what = TASK_A;
+        looperThread.handler.sendMessage(msg);
 
-        @Override
-        public void run() {
-            for (int i = 0; i < seconds; i++) {
-
-                Log.d(TAG, "startThread: " + i);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        /*Handler threadHandler = new Handler(looperThread.looper);
+        threadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    Log.d(TAG, "run: " + i);
+                    SystemClock.sleep(1000);
                 }
             }
-        }
+        });*/
     }
 
-    class ExampleRunnable implements Runnable {
-
-        int seconds;
-
-        public ExampleRunnable(int seconds) {
-            this.seconds = seconds;
-        }
-
-        @Override
-        public void run() {
-            for (int i = 0; i < seconds; i++) {
-
-                if(stopThread) {
-                    return;
-                }
-                if (i == 5) {
-                   /* Handler mainHandler = new Handler(Looper.getMainLooper());
-
-                    mainHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            buttonStartThread.setText("50%");
-                        }
-                    });*/
-
-                   /*buttonStartThread.post(new Runnable() {
-                       @Override
-                       public void run() {
-                            buttonStartThread.setText("50%");
-                       }
-                   });*/
-
-                   runOnUiThread(new Runnable() {
-                       @Override
-                       public void run() {
-                           buttonStartThread.setText("50%");
-                       }
-                   });
-                }
-
-                Log.d(TAG, "startThread: " + i);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+    public void taskB(View view) {
+        Message msg = Message.obtain();
+        msg.what = TASK_B;
+        looperThread.handler.sendMessage(msg);
     }
+
+
 }
